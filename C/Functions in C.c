6430e35fe02a54362b540f9417c1ebea493cc952
carlos echeverria <echeverriadev@gmail.com>
2019-10-18 01:35:13 -0400

    last commit today

---
+++ C/Functions in C.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a,int b,int c,int d){
+    int m1,m2;
+    int ans = (m1=a>b?a:b)>(m2=c>d?c:d)?m1:m2;
+    return ans;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
